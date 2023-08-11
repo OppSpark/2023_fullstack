086db350fb3437e46b4674e1e3b6b53a14a5be13
OppSpark <picks5@naver.com>
2023-08-11 23:02:28 +0900

    db

---
+++ HTTPSSS/db/db_info.js
@@ -0,0 +1,20 @@
+//디비 정보 불러오는 내용
+const dbConfig = {
+	host: 'localhost', 
+	user: 'OppSpark',
+	port: 3306,
+	password: '1515', 
+	database: '23_S', 
+  };
+  
+  // MySQL 데이터베이스 연결 설정
+  const connection = mysql.createConnection(dbConfig);
+  
+  // 데이터베이스 연결 확인
+  connection.connect((err) => {
+	if (err) {
+	  console.error('Error connecting to database: ', err);
+	  return;
+	}
+	console.log('Connected to database!');
+  });

15440c565d5101bdd6be2ab3dde0b5f9d5860b83
OppSpark <picks5@naver.com>
2023-08-15 17:42:11 +0900

    last up

--- HTTPSSS/index.js
+++ HTTPSSS/index.js
@@ -186,9 +186,9 @@ app.post('/login', (req, res) => {
 
 app.get('/post', (req, res) => {
 	let sql = 'SELECT post_title, post_content FROM post_data';
-	db.query(sql, (err, posts) => {
+	db.query(sql, (err, post) => {
 	  if (err) throw err;
-	  res.render('board', { posts: posts });
+	  res.render('board', { post: post });
 	});
   });
 

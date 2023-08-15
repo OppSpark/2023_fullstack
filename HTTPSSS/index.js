dd450e80d858c6b12e938e3905d5a48c2851b057
OppSpark <picks5@naver.com>
2023-08-15 17:41:05 +0900

    last up

--- HTTPSSS/index.js
+++ HTTPSSS/index.js
@@ -17,6 +17,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 
 app.use(express.static(path.join(__dirname,'views')));
 
